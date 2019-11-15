68fd509e8224a69ccc9493c5db12ff3ff1a77b7c
vinod <vinod@PTMM05-2.local>
2019-11-15 10:58:37 +0530

    Small changes in  ui

--- src/app/challenge/add-challenge/add-challenge.component.ts
+++ src/app/challenge/add-challenge/add-challenge.component.ts
@@ -8,57 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AddChallengeComponent implements OnInit {
 
     public routeData: any[] = [];
-    public arryJson = [
-        {
-            "id": 2,
-            "title": "Login",
-            "hierarchy": 1,
-            "parent": 1
-        },
-        {
-            "id": 3,
-            "title": "signup",
-            "hierarchy": 1,
-            "parent": 1
-        },
-        {
-            "id": 4,
-            "title": "Dashboard",
-            "hierarchy": 1,
-            "parent": 1
-        },
-        {
-            "id": 5,
-            "title": "Header",
-            "hierarchy": 2,
-            "parent": 2
-        },
-        {
-            "id": 6,
-            "title": "Footer",
-            "hierarchy": 3,
-            "parent": 3
-        },
-        {
-            "id": 7,
-            "title": "SideMenu",
-            "hierarchy": 4,
-            "parent": 4
-        },
-        {
-            "id": 8,
-            "title": "Routing",
-            "hierarchy": 5,
-            "parent": 5
-        },
-        {
-            "id": 9,
-            "title": "Dropbox",
-            "hierarchy": 6,
-            "parent": 6
-        }
-    ];
-
+    
     constructor() { }
 
     ngOnInit() {
